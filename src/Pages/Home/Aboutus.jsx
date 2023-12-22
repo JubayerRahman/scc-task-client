4ad46b79821c8bce733466c694388d29c7e93d24
JubayerRahman <jubayerr398@gmail.com>
2023-12-22 19:32:29 +0600

    final touchups

--- src/Pages/Home/Aboutus.jsx
+++ src/Pages/Home/Aboutus.jsx
@@ -14,7 +14,7 @@ const Aboutus = () => {
       }, [])
     console.log(services);
   return (
-    <div data-aos="fade-down">
+    <div data-aos="fade-down" className="aboutus">
         <div className='text-white py-[25px] lg:w-[70%] mx-auto p-[10px]'>
       <h1 className='text-2xl'>About us:</h1>
       <p>At SCC, we are at the forefront of revolutionizing task management. With a commitment to streamlining workflows and enhancing productivity, SCC provides innovative solutions that empower individuals and teams to navigate their tasks with unparalleled efficiency. Our cutting-edge technology and user-friendly interfaces create a seamless experience, ensuring that every project, big or small, is handled with precision and ease. Discover a new era of productivity with SCC, where we transform the way you work, one task at a time.</p>
@@ -26,7 +26,7 @@ const Aboutus = () => {
             <div className='flex flex-col lg:flex-row p-[10px] justify-center gap-3 mx-auto'>
                 {
                 services.map(ser=>
-                    <div className="card lg:w-[30%] bg-[#BF3DE2] shadow-xl">
+                    <div className="card bg-opacity-[0.6] lg:w-[30%] bg-[#BF3DE2] shadow-xl">
                     <div className="card-body flex flex-col items-center text-white">
                         <h2 className="card-title">{ser.name}</h2>
                         <p>{ser.price}</p>
--- src/Pages/Home/FAQ.jsx
+++ src/Pages/Home/FAQ.jsx
@@ -8,9 +8,10 @@ const FAQ = () => {
       }, [])
       
   return (
-                <div data-aos="fade-up"  className='p-[10px] flex flex-col gap-3 text-white'>
-                    <h1 className='lg:text-5xl font-[500] my-[50px] text-[#BF3DE2]'>FAQ about us:</h1>
-                <div className="collapse bg-[#BF3DE2]">
+                <div data-aos="fade-up"  className='aboutus p-[10px] flex flex-col gap-3 text-white'>
+                    <h1 className='text-3xl lg:text-5xl font-[500] my-[50px] text-[#BF3DE2]'>FAQ about us:</h1>
+                <div className='flex flex-wrap justify-evenly gap-3 mx-auto'>
+                <div className=" bg-opacity-[0.6] lg:w-[45%] collapse bg-[#BF3DE2]">
             <input type="radio" name="my-accordion-1" /> 
             <div className="collapse-title text-xl font-medium">
             What makes SCC Task Management different from other solutions?
@@ -19,7 +20,7 @@ const FAQ = () => {
                 <p>SCC Task Management stands out with its intuitive interface, customizable workflows, and seamless collaboration features. It offers a user-friendly experience, adapts to various needs, and ensures optimal productivity for individuals and teams.</p>
             </div>
                 </div>
-                <div className="collapse bg-[#BF3DE2]">
+                <div className="bg-opacity-[0.6] lg:w-[45%] collapse bg-[#BF3DE2]">
             <input type="radio" name="my-accordion-1" /> 
             <div className="collapse-title text-xl font-medium">
             Can SCC Task Management integrate with other tools we use?
@@ -28,7 +29,7 @@ const FAQ = () => {
                 <p>Yes, SCC seamlessly integrates with popular productivity tools, creating a cohesive work environment. Connect with calendars, communication platforms, and file-sharing services to centralize your workflow and enhance collaboration.</p>
             </div>
                 </div>
-                <div className="collapse bg-[#BF3DE2]">
+                <div className="bg-opacity-[0.6] lg:w-[45%] collapse bg-[#BF3DE2]">
             <input type="radio" name="my-accordion-1" /> 
             <div className="collapse-title text-xl font-medium">
             How secure is our data with SCC Task Management?
@@ -37,7 +38,7 @@ const FAQ = () => {
                 <p>We take the security of your data seriously. SCC Task Management employs robust security measures to ensure the confidentiality and integrity of your information. Your data is protected against unauthorized access and breaches.</p>
             </div>
                 </div>
-                <div className="collapse bg-[#BF3DE2]">
+                <div className="bg-opacity-[0.6] lg:w-[45%] collapse bg-[#BF3DE2]">
             <input type="radio" name="my-accordion-1" /> 
             <div className="collapse-title text-xl font-medium">
             Is there a trial period for SCC Task Management?
@@ -46,7 +47,7 @@ const FAQ = () => {
                 <p>Yes, we offer trial periods for all our plans. The duration varies by plan, allowing you to explore the features and benefits of SCC Task Management. Take advantage of the trial to experience firsthand how SCC can enhance your task management.</p>
             </div>
                 </div>
-                <div className="collapse bg-[#BF3DE2]">
+                <div className="bg-opacity-[0.6] lg:w-[45%] collapse bg-[#BF3DE2]">
             <input type="radio" name="my-accordion-1" /> 
             <div className="collapse-title text-xl font-medium">
             What level of support is provided with SCC Task Management plans?
@@ -54,6 +55,7 @@ const FAQ = () => {
             <div className="collapse-content"> 
                 <p>SCC Task Management offers different support levels based on your plan. Standard Support is available for the Basic Plan, Priority Support for the Standard Plan, and Premium Support for the Premium Plan. Premium Support includes 24/7 assistance and priority access to new features.</p>
             </div>
+                </div>
                 </div>
     </div>
   )
